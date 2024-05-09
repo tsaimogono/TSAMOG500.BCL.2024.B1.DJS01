01d58b715cbb5cecf91bf7fe5af2ae4da9209719
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-10 01:52:32 +0200

    Ceate an object with physical quantities

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
  * 1. Make the code more readable
  * 2. Pick up calculation errors
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- 
+ */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
-
-
+const props = {
+  vel : [10000,'km/h'], // velocity (km/h)
+  acc : [3,'m/s^2'], // acceleration (m/s^2)
+  time : [3600,'s'], // seconds (1 hour)
+  d : [0,'km'], // distance (km)
+  fuel : [5000,'kg'], // remaining fuel (kg)
+  fbr : [0.5,'kg/s'] // fuel burn rate (kg/s)
+ }
 const d2 = d + (vel*time) //calcultes new distance
 
 const rf = fbr*time //calculates remaining fuel
@@ -27,4 +27,4 @@ calcNewVel = (vel, acc, time) => {
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-*/
+
